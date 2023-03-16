2182abc3716498f23d8d69934e17e39854f267b3
Security & Stuff <74931194+SecurityAndStuff@users.noreply.github.com>
2023-03-16 14:55:06 +0000

    python2 version

--- src/codeql_jenkins.py
+++ src/codeql_jenkins.py
@@ -1,5 +1,5 @@
 import logging
-import urllib.request
+import urllib
 import sys
 import subprocess
 import os
@@ -14,7 +14,7 @@ class Scan:
     IS_WINDOWS = os.name == "nt"
     THREADS = 8
     CODEQL_BUNDLE_URL = (
-        f"https://github.com/github/codeql-action/releases/download/codeql-bundle-20230304/codeql-bundle-"
+        "https://github.com/github/codeql-action/releases/download/codeql-bundle-20230304/codeql-bundle-"
         + ("win64" if IS_WINDOWS else "linux64")
         + ".tar.gz"
     )
@@ -27,15 +27,15 @@ class Scan:
         logging.info("Looking for CodeQL")
         codeql_path = self.DEFAULT_CODEQL_PATH
         if not os.path.exists(self.DEFAULT_CODEQL_PATH):
-            logging.info(f"Didn't find CodeQL in {self.DEFAULT_CODEQL_PATH}")
+            logging.info("Didn't find CodeQL in {}".format(self.DEFAULT_CODEQL_PATH))
             codeql_path = os.path.join(Scan.DEFAULT_PATH, "codeql")
         if not os.path.exists(codeql_path) and Scan.IS_WINDOWS:
-            logging.info(f"Didn't find CodeQL in {codeql_path}")
+            logging.info("Didn't find CodeQL in {}".format(codeql_path))
             codeql_path = os.path.join(Scan.PROGRAM_FILES, "codeql")
         if not os.path.exists(codeql_path):
-            logging.info(f"Didn't find CodeQL in {codeql_path}")
+            logging.info("Didn't find CodeQL in {}".format(codeql_path))
             logging.info("Dowloading codeql")
-            urllib.request.urlretrieve(Scan.CODEQL_BUNDLE_URL, Scan.CODEQL_TAR_FILENAME)
+            urllib.urlretrieve(Scan.CODEQL_BUNDLE_URL, Scan.CODEQL_TAR_FILENAME)
             logging.info("Extracting codeql")
             with tarfile.open(Scan.CODEQL_TAR_FILENAME, "r") as tar:
                 tar.extractall()
@@ -43,11 +43,11 @@ class Scan:
         self.codeql_path_executable = os.path.abspath(
             os.path.join(codeql_path, "codeql.cmd")
         )
-        logging.info(f"Using CodeQL from {self.codeql_path_executable}")
+        logging.info("Using CodeQL from {}".format(self.codeql_path_executable))
 
     def create_database(self, build_command, db_name, source_root, language):
         logging.info("Creating database")
-        subprocess.run(
+        subprocess.call(
             [
                 self.codeql_path_executable,
                 "database",
@@ -67,7 +67,7 @@ class Scan:
 
     def analyze_database(self, db_name, queries, sarif_output_name):
         logging.info("Analyzing database")
-        subprocess.run(
+        subprocess.call(
             [
                 self.codeql_path_executable,
                 "database",
@@ -101,10 +101,10 @@ if __name__ == "__main__":
     print(sys.argv)
     if len(sys.argv) != 7:
         logging.error(
-            f"""Usage: codeql_jenkins.py "source_root" "build_command" "codeql_db_name" "language" "queries" "sarif-output" """
+            """Usage: codeql_jenkins.py "source_root" "build_command" "codeql_db_name" "language" "queries" "sarif-output" """
         )
         logging.info(
-            f"""Example: python codeql_jenkins.py "./app" "dotnet build" "codeql-db-app" "csharp" "codeql/csharp-queries" "codeql-results.sarif" """
+            """Example: python codeql_jenkins.py "./app" "dotnet build" "codeql-db-app" "csharp" "codeql/csharp-queries" "codeql-results.sarif" """
         )
         sys.exit(-1)
     (
@@ -120,4 +120,4 @@ if __name__ == "__main__":
     scan.retrieve_codeql()
     scan.create_database(build_command, db_name, source_root, language)
     scan.analyze_database(db_name, queries, sarif_output_name)
-    logging.info(f"Wrote sarif to {sarif_output_name}")
+    logging.info("Wrote sarif to {}".format(sarif_output_name))
