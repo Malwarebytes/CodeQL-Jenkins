869cf947fe990be47c32f843b03bee359a72cf0e
Security & Stuff <74931194+SecurityAndStuff@users.noreply.github.com>
2023-03-27 15:33:25 +0100

    don't access non existing key

--- src/codeql_jenkins.py
+++ src/codeql_jenkins.py
@@ -9,7 +9,7 @@ logging.getLogger().setLevel(level=logging.INFO)
 
 class Scan:
     DEFAULT_PATH = os.path.expanduser("~")
-    PROGRAM_FILES = os.environ["ProgramFiles"]
+    PROGRAM_FILES = os.environ["ProgramFiles"] if 'Program Files' in os.environ  else DEFAULT_PATH
     IS_WINDOWS = os.name == "nt"
     THREADS = 8
     CODEQL_BUNDLE_URL = (
